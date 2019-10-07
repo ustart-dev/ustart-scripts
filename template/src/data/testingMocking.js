9fa94fd4e2b59e26a1d87d14750462737533151e
Leonel Peña <leo.dhpl@gmail.com>
2019-10-07 14:10:20 -0300

    refactor: replace NODE_ENV from "testing" to "test".

--- template/src/data/testingMocking.js
+++ template/src/data/testingMocking.js
@@ -1,7 +1,7 @@
 import { ustart } from "ustart";
 
 /**
- * Populates the databases at starting time only with NODE_ENV set to "TESTING".
+ * Populates the databases at starting time only with NODE_ENV set to "test".
  * Use it for testing purposes.
  */
 export async function testingMocking() {
